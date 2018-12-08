6575a642226727b0bddadf3f660ae8a0f1d00821
yackoa <yackoa@gmail.com>
2018-12-08 00:40:56 +0000

    removed typo comments

--- script.js
+++ script.js
@@ -1,33 +1,7 @@
 /*
-just append the url 
-*/
-//document.location.hash = "?share=1";
-
-
-//window.history.pushState("object or string", "quora answer without login", "?share=1");
-//document.location.hash = "?share=1";
-//window.location.href
-//window.location.reload()
-//window.location.reload()
-//window.stop()
-/*
-function my_code(){
-//alert(" Alert inside my_code function");
-//var text_val = document.getElementById("t2");
-//text_val.select();
-window.history.pushState("object or string", "quora answer without login", "?share=1");
-window.location.reload()
-window.stop()
-}
-
-window.onload=my_code();
+just append the url and reload it 
 */
 
-//window.location.search += 'share=1';
-//window.stop()
-//document.location.hash = ""
-//window.location.reload()
-//window.stop()
 
 var url = window.location.href;    
 if (url.indexOf('?') > -1){
