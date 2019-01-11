7e9837ba79547b2f1cc3749bcbadf9327f7d3570
yackoa <yackoa@gmail.com>
2019-01-11 01:24:02 +0000

    added code to check user login

--- script.js
+++ script.js
@@ -3,10 +3,19 @@ just append the url and reload it
 */
 
 
-var url = window.location.href;    
-if (url.indexOf('?') > -1){
-   window.stop()
-}else{
-   url += '?magic=1'
-   window.location.href = url;
+var url = window.location.href;
+var oSerializer = new XMLSerializer();
+var sXML = oSerializer.serializeToString(document);
+var userLoggedIn = sXML.includes('<span class="expanded">Notifications');
+
+//console.log(n)
+//console.log(typeof n);
+
+if (!userLoggedIn){
+	if (url.indexOf('?') > -1){
+   	window.stop()
+	}else{
+   		url += '?share=1'
+   		window.location.href = url;
+	}
 }
